f90433b10a4b0ec0d4e6fa5b1c47613b2bf566d7
Gustavo Alencar <gustavo.filho@car10.com.br>
2025-07-22 17:56:12 -0300

    Checkbox

--- JS-para-web/index.js
+++ JS-para-web/index.js
@@ -1,8 +1,10 @@
 const inputItem = document.getElementById("input-item")
 
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
     if (inputItem.value === "") {
         alert("Por favor, insira um item!");
         return
@@ -11,4 +13,7 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const itemDaLista = document.createElement("li");
     const containerItemDaLista = document.createElement("div");
     containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
 })
